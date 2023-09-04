971a62a1b42905828b748519981a633fdcc8f3b6
devodevM <decodev.contact@gmail.com>
2023-09-04 16:47:52 +0100

    Initial Commit

---
+++ NtionalGeography/NtionalGeographyApp.swift
@@ -0,0 +1,17 @@
+//
+//  NtionalGeographyApp.swift
+//  NtionalGeography
+//
+//  Created by Yacine on 9/4/23.
+//
+
+import SwiftUI
+
+@main
+struct NtionalGeographyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
